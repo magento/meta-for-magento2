dee87febb7ba57a948027446a05092ba6939cc13
Dima Dromov <dimadidr@gmail.com>
2023-02-01 20:43:19 +0200

    Remove absent helper method

--- app/code/Meta/Conversion/Test/Unit/Observer/AddToCartTest.php
+++ app/code/Meta/Conversion/Test/Unit/Observer/AddToCartTest.php
@@ -77,7 +77,6 @@ class AddToCartTest extends CommonTest
         $product->setName('Earphones');
         $this->request->method('getParam')->willReturn($sku);
         $this->magentoDataHelper->method('getProductBySku')->willReturn($product);
-        $this->magentoDataHelper->method('getProductById')->willReturn($product);
 
         $observer = new Observer(['eventId' => $eventId]);
 
