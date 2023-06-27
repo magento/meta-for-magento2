359eb888c4fd4f183d68c06e727eaa2b8abf931d
atuld123 <131775408+atuld123@users.noreply.github.com>
2023-06-27 18:23:17 +0100

    created data patch for  updating source model value from Facebook to … (#274)
    * created data patch for  updating source model value from Facebook to Meta
    
    * removed echo statement
    
    ---------
    
    Co-authored-by: Atul Dusane <atuldusane@fb.com>

---
+++ app/code/Meta/Catalog/Setup/Patch/Data/UpdateMetaCatalogSourceAttribute.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Meta\Catalog\Setup\Patch\Data;
+
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class UpdateMetaCatalogSourceAttribute implements DataPatchInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+    }
+
+    /**
+     * Get dependencies for the data patch
+     *
+     * @return array
+     */
+    public static function getDependencies(): array
+    {
+        return  [];
+    }
+
+    /**
+     * Get alias for the data patch
+     *
+     * @return array
+     */
+    public function getAliases(): array
+    {
+        return [];
+    }
+
+    /**
+     * Update the attribute source model value from Facebook to Meta
+     *
+     * @return void
+     */
+    public function apply(): void
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+
+        $this->updateAttribute();
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * Update the attribute source model value from Facebook to Meta
+     */
+    private function updateAttribute()
+    {
+        $connection = $this->moduleDataSetup->getConnection();
+        $tableName = $this->moduleDataSetup->getTable('eav_attribute');
+        $oldSourceModel = 'Facebook\BusinessExtension\Model\Config\Source\Product\GoogleProductCategory';
+        $newSourceModel = \Meta\Catalog\Model\Config\Source\Product\GoogleProductCategory::class;
+
+        $connection->update(
+            $tableName,
+            ['source_model' => $newSourceModel],
+            $connection->quoteInto('source_model = ?', $oldSourceModel)
+        );
+    }
+
+    /**
+     * Revert the attribute source model value from Facebook to Meta
+     *
+     * @return void
+     */
+    public function revert(): void
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+
+        $this->revertAttributeUpdate();
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * Revert the attribute source model value from Facebook to Meta
+     */
+    private function revertAttributeUpdate()
+    {
+        $connection = $this->moduleDataSetup->getConnection();
+        $tableName = $this->moduleDataSetup->getTable('eav_attribute');
+        $oldSourceModel = 'Facebook\BusinessExtension\Model\Config\Source\Product\GoogleProductCategory';
+        $newSourceModel = \Meta\Catalog\Model\Config\Source\Product\GoogleProductCategory::class;
+        $connection->update(
+            $tableName,
+            ['source_model' => $oldSourceModel],
+            $connection->quoteInto('source_model = ?', $newSourceModel)
+        );
+    }
+}
