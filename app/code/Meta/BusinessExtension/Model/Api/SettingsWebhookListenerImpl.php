29c1ff0d27a23cdb85d069282633f4b583ce0912
Yang Ming <yangssr@meta.com>
2023-09-21 17:38:13 -0700

    Integrate catalog update script (#437)
    * integrate with catalog-update script
    
    * refactor exception and remove log
    
    * lint
    
    ---------
    
    Co-authored-by: Yang Ming <yangssr@fb.com>

--- app/code/Meta/BusinessExtension/Model/Api/SettingsWebhookListenerImpl.php
+++ app/code/Meta/BusinessExtension/Model/Api/SettingsWebhookListenerImpl.php
@@ -22,14 +22,16 @@ namespace Meta\BusinessExtension\Model\Api;
 
 use Exception;
 use Magento\Config\Model\ResourceModel\Config\Data\CollectionFactory;
-use Magento\Store\Model\StoreManagerInterface;
+use Magento\Framework\Exception\LocalizedException;
 use Meta\BusinessExtension\Api\CustomApiKey\UnauthorizedTokenException;
 use Meta\BusinessExtension\Api\SettingsWebhookListenerInterface;
 use Meta\BusinessExtension\Api\SettingsWebhookRequestInterface;
-use Meta\BusinessExtension\Cron\UpdateMBESettings;
+use Meta\BusinessExtension\Helper\CatalogConfigUpdateHelper;
 use Meta\BusinessExtension\Helper\FBEHelper;
+use Meta\BusinessExtension\Helper\GraphAPIAdapter;
 use Meta\BusinessExtension\Model\Api\CustomApiKey\Authenticator;
 use Meta\BusinessExtension\Model\System\Config as SystemConfig;
+use Throwable;
 
 class SettingsWebhookListenerImpl implements SettingsWebhookListenerInterface
 {
@@ -39,49 +41,50 @@ class SettingsWebhookListenerImpl implements SettingsWebhookListenerInterface
     private SystemConfig $systemConfig;
 
     /**
-     * @var StoreManagerInterface
-     */
-    private StoreManagerInterface $storeManager;
-
-    /**
      * @var FBEHelper
      */
     private FBEHelper $fbeHelper;
 
     /**
-     * @var UpdateMBESettings
+     * @var CatalogConfigUpdateHelper
      */
-    private UpdateMBESettings $updateMBESettings;
+    private CatalogConfigUpdateHelper $catalogConfigUpdateHelper;
 
     /**
      * @var CollectionFactory
      */
-    private $collectionFactory;
+    private CollectionFactory $collectionFactory;
 
     /** @var Authenticator */
     private Authenticator $authenticator;
 
     /**
-     * @param StoreManagerInterface $storeManager
-     * @param UpdateMBESettings $updateMBESettings
+     * @var GraphAPIAdapter
+     */
+    private GraphAPIAdapter $graphApiAdapter;
+
+    /**
      * @param SystemConfig $systemConfig
      * @param FBEHelper $fbeHelper
      * @param CollectionFactory $collectionFactory
+     * @param Authenticator $authenticator
+     * @param CatalogConfigUpdateHelper $catalogConfigUpdateHelper
+     * @param GraphAPIAdapter $graphApiAdapter
      */
     public function __construct(
-        StoreManagerInterface $storeManager,
-        UpdateMBESettings $updateMBESettings,
-        SystemConfig          $systemConfig,
-        FBEHelper             $fbeHelper,
-        CollectionFactory $collectionFactory,
-        Authenticator $authenticator
+        SystemConfig              $systemConfig,
+        FBEHelper                 $fbeHelper,
+        CollectionFactory         $collectionFactory,
+        Authenticator             $authenticator,
+        CatalogConfigUpdateHelper $catalogConfigUpdateHelper,
+        GraphAPIAdapter           $graphApiAdapter
     ) {
-        $this->storeManager = $storeManager;
         $this->systemConfig = $systemConfig;
-        $this->updateMBESettings = $updateMBESettings;
         $this->fbeHelper = $fbeHelper;
         $this->collectionFactory = $collectionFactory;
         $this->authenticator = $authenticator;
+        $this->catalogConfigUpdateHelper = $catalogConfigUpdateHelper;
+        $this->graphApiAdapter = $graphApiAdapter;
     }
 
     /**
@@ -90,6 +93,7 @@ class SettingsWebhookListenerImpl implements SettingsWebhookListenerInterface
      * @param SettingsWebhookRequestInterface[] $settingsWebhookRequest
      * @return void
      * @throws UnauthorizedTokenException
+     * @throws LocalizedException
      */
     public function processSettingsWebhookRequest(array $settingsWebhookRequest): void
     {
@@ -103,40 +107,87 @@ class SettingsWebhookListenerImpl implements SettingsWebhookListenerInterface
      * Process webhook request
      *
      * @param SettingsWebhookRequestInterface $setting
+     * @throws LocalizedException
      */
     private function updateSetting(SettingsWebhookRequestInterface $setting): void
     {
-        // Step 1 - Get StoreId by business_extension_id
-        $external_business_id = $setting->getExternalBusinessId();
-        $installedConfigs = $this->getMBEInstalledConfigsByExternalBusinessId($external_business_id);
 
+        // Step 1 - Get StoreId by business_extension_id
+        $externalBusinessId = $setting->getExternalBusinessId();
+        $installedConfigs = $this->getMBEInstalledConfigsByExternalBusinessId($externalBusinessId);
         if (empty($installedConfigs)) {
-            $this->fbeHelper->log(
-                'Skipping update MBESettings. No store id is found for external_business_id: {$external_business_id}'
-            );
-            return;
+            $this->throwException('No store id is found for found for external_business_id: '.$externalBusinessId);
         }
         // StoreId and externalBusinessId is 1:1 mapping, hence get $storeIds[0] as $storeId in below.
         $storeId = $installedConfigs[0]->getScopeId();
-        // Step 2
-        // - Trigger Magento polling Graph API fbe_install,
-        // - Store latest MBESettings in magento DB
-        // (To see details of what config stored, find in SaveFBEInstallResponse->Save() function)
-        $this->updateMBESettings->updateMBESettingsByStoreId((int)$storeId);
 
-        // Step 3 TODO:
-        // calling Catalog_Script(catalogId, pixel_id, storeId);
-        $this->systemConfig->getCatalogId();
-        $this->systemConfig->getPixelId();
+        // Step 2 - Trigger Magento polling Graph API fbe_install,
+        $fbeResponse = $this->getMBESettings((int)$storeId);
+        // Step 3 - calling Catalog Script to update
+        $this->catalogConfigUpdateHelper
+            ->updateCatalogConfiguration(
+                (int)$storeId,
+                $fbeResponse['catalog_id'],
+                $fbeResponse['commerce_partner_integration_id'],
+                $fbeResponse['pixel_id'],
+            );
+
+        // Step 4 - Verify Catalog id updated correctly
+        if ($this->systemConfig->getCatalogId() !== $fbeResponse['catalog_id']) {
+            $this->throwException('Catalog config update failed for external_business_id: '.$externalBusinessId);
+        }
+    }
+
+    /**
+     * Polling from fbe_install Graph API
+     *
+     * @param int $storeId
+     * @return string[]
+     * @throws LocalizedException
+     */
+    private function getMBESettings(int $storeId): array
+    {
+        try {
+            $accessToken = $this->systemConfig->getAccessToken($storeId);
+            $businessId = $this->systemConfig->getExternalBusinessId($storeId);
+            if (!$accessToken || !$businessId) {
+                $this->throwException('AccessToken or BusinessID not found for storeID:'.$storeId);
+            }
+            $response = $this->graphApiAdapter->getFBEInstalls($accessToken, $businessId);
+            if (!is_array($response) || empty($response)) {
+                $this->throwException('Skipping FBEInstalls save. Response format is incorrect.');
+            }
+            $data = $response['data'][0];
+            return [
+                'catalog_id' => $data['catalog_id'] ?? '',
+                'commerce_partner_integration_id' => $data['commerce_partner_integration_id'] ?? '',
+                'pixel_id' => $data['pixel_id'] ?? '',
+            ];
+        } catch (Throwable $e) {
+            $this->throwException('Failed to pull fbe response'.$e->getMessage());
+        }
+    }
+
+    /**
+     * Exception helper
+     *
+     * @param string $errorMessage
+     * @throws LocalizedException
+     */
+    private function throwException(string $errorMessage)
+    {
+        throw new LocalizedException(__(
+            $errorMessage
+        ));
     }
 
     /**
      * Get config values where MBE is installed for external_business_Id
      *
-     * @param string $external_business_id
+     * @param string $externalBusinessId
      * @return array
      */
-    private function getMBEInstalledConfigsByExternalBusinessId(string $external_business_id): array
+    private function getMBEInstalledConfigsByExternalBusinessId(string $externalBusinessId): array
     {
         try {
             $collection = $this->collectionFactory->create();
@@ -146,11 +197,11 @@ class SettingsWebhookListenerImpl implements SettingsWebhookListenerInterface
                     'path',
                     ['eq' => SystemConfig::XML_PATH_FACEBOOK_BUSINESS_EXTENSION_EXTERNAL_BUSINESS_ID]
                 )
-                ->addValueFilter($external_business_id)
+                ->addValueFilter($externalBusinessId)
                 ->addFieldToSelect('scope_id');
 
             return $collection->getItems();
-          
+
         } catch (Exception $e) {
             $this->fbeHelper->logException($e);
             return [];
