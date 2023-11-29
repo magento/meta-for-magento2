3c4c7e90717e7b9325e833bcde4f103f325283f1
Steve Gordon <stevegordon@meta.com>
2023-11-29 08:31:22 -0800

    Deleted code reverted (#568) (#569)
    Co-authored-by: singhramanpreett <129768663+singhramanpreett@users.noreply.github.com>

--- app/code/Meta/Catalog/Plugin/FacebookCatalogDeletePlugin.php
+++ app/code/Meta/Catalog/Plugin/FacebookCatalogDeletePlugin.php
@@ -80,9 +80,15 @@ class FacebookCatalogDeletePlugin
      */
     public function afterDelete(Product $subject, $result, $object)
     {
+        $identifier = $this->identifier->getMagentoProductRetailerId($object);
+        if ($identifier === false) {
+            $this->fbeHelper->log('Deleted Product does not have meta identifier.');
+            return $result;
+        }
+
         $catalogDelete = $this->catalogUpdateFactory->create()
             ->setProductId($object->getId())
-            ->setSku($object->getSku())
+            ->setSku($identifier)
             ->setMethod('delete');
         try {
             $this->catalogUpdateResourceModel->deleteUpdateProductEntries($identifier);
