322211007c42a7ce8a4d6b664bcba4bf8f78c8c6
Paul Kang <sollipse@meta.com>
2024-03-23 22:00:58 -0700

    Changing back version number

--- app/code/Meta/BusinessExtension/Model/System/Config.php
+++ app/code/Meta/BusinessExtension/Model/System/Config.php
@@ -163,7 +163,7 @@ class Config
      *
      * @var string|null
      */
-    private ?string $version = '1.3.1-dev';
+    private ?string $version = '1.3.0';
 
     /**
      * @method __construct
