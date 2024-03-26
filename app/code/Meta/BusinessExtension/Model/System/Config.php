36c120118336cac54dc45ae3a87b02c5369f291e
miroshni <miroshni@adobe.com>
2024-03-26 09:36:48 -0500

    Meta 1.3.0 release - change version for main branch

--- app/code/Meta/BusinessExtension/Model/System/Config.php
+++ app/code/Meta/BusinessExtension/Model/System/Config.php
@@ -163,7 +163,7 @@ class Config
      *
      * @var string|null
      */
-    private ?string $version = '1.3.0';
+    private ?string $version = '1.3.1-dev';
 
     /**
      * @method __construct
