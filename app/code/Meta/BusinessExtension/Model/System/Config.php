e9812a97d7123e6cdce63c6d63c6132b3be6f762
Alex Zarichnyi <zlik@fb.com>
2024-09-06 11:38:00 -0700

    updated version in 1.3.3

--- app/code/Meta/BusinessExtension/Model/System/Config.php
+++ app/code/Meta/BusinessExtension/Model/System/Config.php
@@ -163,7 +163,7 @@ class Config
      *
      * @var string|null
      */
-    private ?string $version = '1.3.3-dev';
+    private ?string $version = '1.3.3';
 
     /**
      * @method                                       __construct
