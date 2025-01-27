aa95f7be9e348141792e1c320d416352e279ad8b
nrostrow-meta <132953571+nrostrow-meta@users.noreply.github.com>
2025-01-27 15:20:18 -0600

    Write ratings and reviews data to CSV file (#854)
    * Fetching R&R data to sync through feed upload API. First step accomplished here is fetching the required data.
    
    * Removing log call
    
    * Writing ratings and reviews data to CSV file.
    
    * Fixing static test issues and missing dependency
    
    * Fixing some static test issues.
    
    * Adding module-review to composer.json file.
    
    * Addressing comments.
    
    ---------
    
    Co-authored-by: Noah Ostrowski <nrostrow@fb.com>

--- app/code/Meta/Catalog/Model/Product/Feed/Method/RatingsAndReviewsFeedApi.php
+++ app/code/Meta/Catalog/Model/Product/Feed/Method/RatingsAndReviewsFeedApi.php
@@ -25,12 +25,18 @@ use Meta\BusinessExtension\Helper\FBEHelper;
 use Meta\BusinessExtension\Model\System\Config as SystemConfig;
 use Meta\Catalog\Model\Product\Feed\Builder\Tools as BuilderTools;
 use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Framework\Exception\FileSystemException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Filesystem;
 use Magento\Review\Model\ResourceModel\Review\CollectionFactory as ReviewCollectionFactory;
 use Magento\Review\Model\ResourceModel\Rating\Option\Vote\CollectionFactory as VoteCollectionFactory;
 use Magento\Review\Model\Review;
 
 class RatingsAndReviewsFeedApi
 {
+    private const FEED_FILE_NAME = 'meta_ratings_and_reviews%s.csv';
+    private const VAR_DIR = 'var';
+
     /**
      * @var int
      */
@@ -57,6 +63,11 @@ class RatingsAndReviewsFeedApi
     private ProductRepositoryInterface $productRepository;
 
     /**
+     * @var Filesystem
+     */
+    protected Filesystem $fileSystem;
+
+    /**
      * @var ReviewCollectionFactory
      */
     private ReviewCollectionFactory $reviewCollectionFactory;
@@ -71,6 +82,7 @@ class RatingsAndReviewsFeedApi
      * @param SystemConfig $systemConfig
      * @param BuilderTools $builderTools
      * @param ProductRepositoryInterface $productRepository
+     * @param Filesystem $filesystem
      * @param ReviewCollectionFactory $reviewCollectionFactory
      * @param VoteCollectionFactory $voteCollectionFactory
      */
@@ -79,6 +91,7 @@ class RatingsAndReviewsFeedApi
         SystemConfig                $systemConfig,
         BuilderTools                $builderTools,
         ProductRepositoryInterface  $productRepository,
+        Filesystem                  $filesystem,
         ReviewCollectionFactory     $reviewCollectionFactory,
         VoteCollectionFactory       $voteCollectionFactory
     ) {
@@ -86,6 +99,7 @@ class RatingsAndReviewsFeedApi
         $this->systemConfig = $systemConfig;
         $this->builderTools = $builderTools;
         $this->productRepository = $productRepository;
+        $this->fileSystem = $filesystem;
         $this->reviewCollectionFactory = $reviewCollectionFactory;
         $this->voteCollectionFactory = $voteCollectionFactory;
     }
@@ -140,7 +154,7 @@ class RatingsAndReviewsFeedApi
 
             $ratingsAndReviewsData['reviews'] = $reviews;
 
-            // TODO generate CSV file
+            $this->generateRatingsAndReviewsFeedUploadFile($ratingsAndReviewsData);
 
             // TODO make graph API call with CSV file to sync R&R data
 
@@ -199,5 +213,99 @@ class RatingsAndReviewsFeedApi
             ]
         ];
         return $productData;
+    }
+
+    /**
+     * Generate ratings and reviews feed upload file
+     *
+     * @param array $ratingsAndReviewsData
+     * @return string
+     * @throws FileSystemException
+     * @throws NoSuchEntityException
+     */
+    private function generateRatingsAndReviewsFeedUploadFile($ratingsAndReviewsData): string
+    {
+        $filePath = 'export/' . $this->getFeedFileName();
+        $directory = $this->fileSystem->getDirectoryWrite(self::VAR_DIR);
+        $directory->create('export');
+        $csvString = $this->generateCsvString($ratingsAndReviewsData);
+        $directory->writeFile($filePath, $csvString);
+        return $directory->getAbsolutePath($filePath);
+    }
+
+    /**
+     * Get file name with store code suffix for non-default store (no suffix for default one)
+     *
+     * @return string
+     * @throws NoSuchEntityException
+     */
+    private function getFeedFileName(): string
+    {
+        $defaultStoreId = $this->systemConfig->getStoreManager()->getDefaultStoreView()->getId();
+        $storeCode = $this->systemConfig->getStoreManager()->getStore($this->storeId)->getCode();
+        return sprintf(
+            self::FEED_FILE_NAME,
+            ($this->storeId && $this->storeId !== $defaultStoreId) ? ('_' . $storeCode) : ''
+        );
+    }
+
+    /**
+     * Generate string to write into feed upload file (CSV)
+     *
+     * @param array $ratingsAndReviewsData
+     * @return string
+     */
+    private function generateCsvString($ratingsAndReviewsData): string
+    {
+        $csvString = '';
+        $csvColumns = [
+            "aggregator",
+            "store.name",
+            "store.id",
+            "store.storeUrls",
+            "reviewID",
+            "rating",
+            "title",
+            "content",
+            "createdAt",
+            "country",
+            "reviewer.name",
+            "product.name",
+            "product.url",
+            "product.imageUrls",
+            "product.productIdentifiers.skus"
+        ];
+        $csvRows = [];
+        foreach ($ratingsAndReviewsData['reviews'] as $review) {
+            $row = [
+                $ratingsAndReviewsData['aggregator'],
+                $ratingsAndReviewsData['store']['name'],
+                $ratingsAndReviewsData['store']['id'],
+                json_encode($ratingsAndReviewsData['store']['storeUrls']),
+                $review['reviewID'],
+                $review['rating'],
+                $review['title'],
+                $review['content'],
+                $review['createdAt'],
+                $review['country'],
+                $review['reviewer']['name'],
+                $review['product']['name'],
+                $review['product']['url'],
+                json_encode($review['product']['imageUrls']),
+                json_encode($review['product']['productIdentifiers']['skus'])
+            ];
+            $csvRows[] = $row;
+        }
+        foreach ($csvColumns as $column) {
+            $csvString .= '"' . $column . '",';
+        }
+        $csvString .= "\n";
+        foreach ($csvRows as $row) {
+            foreach ($row as $value) {
+                $csvString .= '"' . $value . '",';
+            }
+            $csvString .= "\n";
+        }
+        return $csvString;
     }
 }
