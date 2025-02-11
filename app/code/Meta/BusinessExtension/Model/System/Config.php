3ead74f4880babf9d36ee22199127101dfb13bf2
pradeepkanaka <pradeepkanaka.h@gmail.com>
2025-02-11 15:17:11 +0530

    Meta update next version

--- app/code/Meta/BusinessExtension/Model/System/Config.php
+++ app/code/Meta/BusinessExtension/Model/System/Config.php
@@ -158,7 +158,7 @@ class Config
      *
      * @var string|null
      */
-    private ?string $version = '1.4.1';
+    private ?string $version = '1.4.2-dev';
 
     /**
      * Config class constructor
