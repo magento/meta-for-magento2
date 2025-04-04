93a00b0018106fe1a10dc0097a29a1e7a39f3723
Priyambad <priyambad.pandey@gmail.com>
2025-04-04 08:33:10 +0530

    ADO-313: customer data for new section

---
+++ app/code/Meta/Conversion/CustomerData/EventData.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Meta\Conversion\CustomerData;
+
+use Magento\Customer\CustomerData\SectionSourceInterface;
+use Magento\Customer\Model\Session as CustomerSession;
+
+class EventData implements SectionSourceInterface
+{
+
+    public function __construct(
+        private readonly CustomerSession $customerSession)
+    { }
+
+    public function getSectionData()
+    {
+        return ['eventIds' => $this->customerSession->getEventIds() ?: []];
+    }
+}
