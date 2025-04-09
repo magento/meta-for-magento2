e0883aa0e2379d20d33467de5f0454bb4bde987e
pradeepkanaka <pradeepkanaka.h@gmail.com>
2025-04-09 15:17:34 +0530

    Meta 1.4.2 release sync

--- app/code/Meta/BusinessExtension/Model/System/Config.php
+++ app/code/Meta/BusinessExtension/Model/System/Config.php
@@ -158,7 +158,7 @@ class Config
      *
      * @var string|null
      */
-    private ?string $version = '1.4.2';
+    private ?string $version = '1.4.3-dev';
 
     /**
      * Config class constructor
