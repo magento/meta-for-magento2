208ce89bdbcab1a24da7e130870121574aea7285
pradeepkanaka <pradeepkanaka.h@gmail.com>
2025-09-03 12:26:47 +0530

    Update version

--- app/code/Meta/BusinessExtension/Model/System/Config.php
+++ app/code/Meta/BusinessExtension/Model/System/Config.php
@@ -155,7 +155,7 @@ class Config
      *
      * @var string|null
      */
-    private ?string $version = '1.6.1';
+    private ?string $version = '1.6.2-dev';
 
     /**
      * Config class constructor
